e693fdf2b0dcc1bafb93aa0460c29b317f8dbfe3
yongj.zhuang <yongj.zhuang@outlook.com>
2024-06-02 00:41:15 +0800

    Document EventPipeline

--- api/event.go
+++ api/event.go
@@ -8,28 +8,30 @@ var (
 	// Reply api.ImageCompressReplyEvent when the processing succeeds.
 	GenImgThumbnailPipeline = miso.NewEventPipeline[ImgThumbnailTriggerEvent]("event.bus.fstore.image.compress.processing").
 				LogPayload().
-				MaxRetry(10)
+				MaxRetry(10).
+				Document("GenImgThumbnailPipeline", "Pipeline to trigger async image thumbnail generation, will reply api.ImageCompressReplyEvent when the processing succeeds.")
 
 	// Pipeline to trigger async video thumbnail generation.
 	//
 	// Reply api.GenVideoThumbnailReplyEvent when the processing succeeds.
 	GenVidThumbnailPipeline = miso.NewEventPipeline[VidThumbnailTriggerEvent]("event.bus.fstore.video.thumbnail.processing").
 				LogPayload().
-				MaxRetry(10)
+				MaxRetry(10).
+				Document("GenVidThumbnailPipeline", "Pipeline to trigger async video thumbnail generation, will reply api.GenVideoThumbnailReplyEvent when the processing succeeds.")
 )
 
 // Event sent to hammer to trigger an vidoe thumbnail generation.
 type VidThumbnailTriggerEvent struct {
-	Identifier string // identifier
-	FileId     string // file id from mini-fstore
-	ReplyTo    string // event bus that will receive event about the generated video thumbnail.
+	Identifier string `desc:"dentifier"`
+	FileId     string `desc:"file id from mini-fstore"`
+	ReplyTo    string `desc:"event bus that will receive event about the generated video thumbnail."`
 }
 
 // Event sent to hammer to trigger an image compression.
 type ImgThumbnailTriggerEvent struct {
-	Identifier string // identifier
-	FileId     string // file id from mini-fstore
-	ReplyTo    string // event bus that will receive event about the compressed image
+	Identifier string `desc:"identifier"`
+	FileId     string `desc:"file id from mini-fstore"`
+	ReplyTo    string `desc:"event bus that will receive event about the generated image thumbnail."`
 }
 
 // Event replied from hammer about the compressed image.
